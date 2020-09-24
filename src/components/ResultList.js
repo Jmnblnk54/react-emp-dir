3d538ebaa59b76108b007452da218df8dcabca8e
Joshua Blank <jmnblnk@yahoo.com>
2020-09-24 16:02:56 -0700

    employees now render to the screen

--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -1,19 +1,32 @@
 import React from "react";
 
-function ResultList(props) {
-    console.log("here are props: ", props);
-  return (
-    <ul className="list-group">
-      {/* {props.map(result => (
-        <li className="list-group-item" key={result.id}>
-          <img alt={result.title} className="img-fluid" src={result.images.original.url} />
-        </li>
-      ))} */}
-      <li>
-
-      </li>
-    </ul>
-  );
-}
-
+// function ResultList(props) {
+//     console.log("here are props: ", props.results);
+//   return (
+    function ResultList(props) {
+        const { results } = props;
+        return (
+          <table>
+            <caption>Employee Directory</caption>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Email</th>
+                <th>Phone</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results.map(results => (
+                <tr key={results.name}>
+                  <td>{results.gender}</td>
+                  <td>{results.email}</td>
+                  <td>{results.phone}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        );
+      }
+  
 export default ResultList;
