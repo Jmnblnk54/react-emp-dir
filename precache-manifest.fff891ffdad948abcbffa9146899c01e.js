949d9a0bbfbc161da375aa1c00d44e98c8f54924
Joshua Blank <jmnblnk@yahoo.com>
2020-10-19 09:16:02 -0700

    Updates

---
+++ precache-manifest.fff891ffdad948abcbffa9146899c01e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "068da8f5e001d40e77aa10c62859d928",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fddf7d78ee100922fa4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14f96444b6b800d2ef4d",
+    "url": "/static/js/2.ca27411b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca27411b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fddf7d78ee100922fa4",
+    "url": "/static/js/main.c3ae1ca3.chunk.js"
+  },
+  {
+    "revision": "896f0a9dab2e31b77c20",
+    "url": "/static/js/runtime-main.36207deb.js"
+  }
+]);
